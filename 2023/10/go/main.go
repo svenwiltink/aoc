fe07dde6f99d7a9e06c82751dbea63864cbc028f
Sven Wiltink <sven@wiltink.dev>
2023-12-10 17:22:28 +0100

    day 10

--- 2023/10/go/main.go
+++ 2023/10/go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/svenwiltink/aoc/common"
 )
@@ -16,6 +17,17 @@ const (
 	ground     = '.'
 	start      = 'S'
 )
+
+func isHairpin(a, b rune) bool {
+	switch {
+	case a == NE && b == NW:
+		fallthrough
+	case a == SE && b == SW:
+		return true
+	default:
+		return false
+	}
+}
 
 var directionChange = map[rune]map[rune]rune{
 	'E': {
@@ -45,7 +57,8 @@ var directionChange = map[rune]map[rune]rune{
 }
 
 func main() {
-	part1()
+	// part1()
+	part2()
 }
 
 func part1() {
@@ -58,13 +71,76 @@ func part1() {
 	steps := 0
 	for {
 		steps++
-		x, y, direction = m.getNextDirection(x, y, direction)
+		x, y, direction = m.traverse(x, y, direction)
 		if x == startX && y == startY {
 			break
 		}
 	}
 
 	fmt.Println(steps / 2)
+}
+
+func part2() {
+	m := maze(common.GetLines())
+
+	startX, startY := m.findStart()
+	direction := m.startDirection()
+
+	visited := make(map[[2]int]bool)
+	visited[[2]int{startX, startY}] = true
+
+	x, y, direction := startX, startY, direction
+	for {
+		x, y, direction = m.traverse(x, y, direction)
+		visited[[2]int{x, y}] = true
+		if x == startX && y == startY {
+			break
+		}
+	}
+
+	// ugly hack to repair the input with the correct pipe
+	m[startY] = strings.ReplaceAll(m[startY], "S", "J")
+
+	count := 0
+	for y, line := range m {
+		var inside bool
+		var previousChar rune
+
+		for x, c := range line {
+			v := visited[[2]int{x, y}]
+
+			if !v {
+				previousChar = '.'
+				if inside {
+					count++
+				}
+				continue
+			}
+
+			switch c {
+			case '|':
+				inside = !inside
+			case '-':
+			default:
+				if previousChar == '.' {
+					previousChar = c
+					inside = !inside
+					continue
+				}
+
+				if isHairpin(previousChar, c) {
+					inside = !inside
+				}
+
+				previousChar = '.' // pretend the corner doesn't exist
+
+			}
+
+		}
+	}
+
+	fmt.Println(count)
+
 }
 
 type maze []string
@@ -105,6 +181,7 @@ func (m maze) startDirection() rune {
 		}
 
 		if newX < 0 || newY < 0 || newX > bounds || newY > bounds {
+			fmt.Println("out of bounds", newX, newY, bounds)
 			continue
 		}
 
@@ -118,7 +195,7 @@ func (m maze) startDirection() rune {
 	panic("no start direction")
 }
 
-func (m maze) getNextDirection(startX, startY int, direction rune) (newX, newY int, newDirection rune) {
+func (m maze) traverse(startX, startY int, direction rune) (newX, newY int, newDirection rune) {
 	newX, newY = startX, startY
 	switch direction {
 	case 'N':

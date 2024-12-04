3ae90866e4a38e6f6628cc62505655ac53d0b5e9
Sven Wiltink <swiltink@transip.nl>
2024-12-04 14:50:27 +0100

    different approach

--- 2024/04/go/main.go
+++ 2024/04/go/main.go
@@ -6,51 +6,32 @@ import (
 )
 
 func main() {
-	part1()
-	part2()
-}
-func part1() {
 	lines := common.GetLines()
 
-	word := `XMAS`
-	var part1 int
+	var part1, part2 int
+
+	part2lookup := make(map[[2]int]bool)
 	for x := range len(lines[0]) {
 		for y := range len(lines) {
-			if lines[y][x] != word[0] {
-				continue
-			}
 			for dx := -1; dx <= 1; dx++ {
 				for dy := -1; dy <= 1; dy++ {
-					if dx == 0 && dy == 0 {
-						continue
-					}
-					remainder := []byte(word[1:])
-					valid := true
-					xLetter := x
-					yLetter := y
-					for _, letter := range remainder {
-						xLetter += dx
-						yLetter += dy
-
-						//bound check
-						if xLetter < 0 || xLetter >= len(lines[0]) {
-							valid = false
-							break
-						}
-
-						if yLetter < 0 || yLetter >= len(lines) {
-							valid = false
-							break
-						}
-
-						if letter != lines[yLetter][xLetter] {
-							valid = false
-							break
-						}
-					}
-
+					valid := checkWord(lines, x, y, "XMAS", dx, dy)
 					if valid {
 						part1++
+					}
+
+					// both dy and dx must be <> 0 in order for it to be a diagonal
+					if dy != 0 && dx != 0 {
+						valid = checkWord(lines, x, y, "MAS", dx, dy)
+						coord := [2]int{x + dx, y + dy}
+						if valid {
+							current := part2lookup[coord]
+							if current {
+								part2++
+							}
+
+							part2lookup[coord] = true
+						}
 					}
 				}
 			}
@@ -58,35 +39,33 @@ func part1() {
 	}
 
 	fmt.Println(part1)
-}
-func part2() {
-	lines := common.GetLines()
-	var part2 int
-	for x := 1; x < len(lines[0])-1; x++ {
-		for y := 1; y < len(lines)-1; y++ {
-			if lines[y][x] != 'A' {
-				continue
-			}
-
-			tl := lines[y-1][x-1]
-			tr := lines[y-1][x+1]
-			bl := lines[y+1][x-1]
-			br := lines[y+1][x+1]
-
-			word := string([]byte{tl, 'A', br})
-			word2 := string([]byte{bl, 'A', tr})
-
-			if word != `MAS` && word != `SAM` {
-				continue
-			}
-
-			if word2 != `MAS` && word2 != `SAM` {
-				continue
-			}
-
-			part2++
-		}
-	}
-
 	fmt.Println(part2)
+}
+
+func checkWord(lines []string, x int, y int, word string, dx int, dy int) bool {
+	valid := true
+	xLetter := x
+	yLetter := y
+	for _, letter := range []byte(word) {
+
+		//bound check
+		if xLetter < 0 || xLetter >= len(lines[0]) {
+			valid = false
+			break
+		}
+
+		if yLetter < 0 || yLetter >= len(lines) {
+			valid = false
+			break
+		}
+
+		if letter != lines[yLetter][xLetter] {
+			valid = false
+			break
+		}
+
+		xLetter += dx
+		yLetter += dy
+	}
+	return valid
 }

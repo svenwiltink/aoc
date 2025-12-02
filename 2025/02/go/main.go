4b8d6e2e5b8e1986caf7b2ff18765e3bf084ed73
Sven Wiltink <swiltink@transip.nl>
2025-12-02 11:13:25 +0100

    part2

--- 2025/02/go/main.go
+++ 2025/02/go/main.go
@@ -11,14 +11,14 @@ type pair [2]int
 
 func main() {
 	ranges := getRanges()
-	fmt.Println(common.Sum(common.Map(checkRange, ranges)))
-
+	fmt.Println(common.Sum(common.Map(common.Curry(checkRange, checkNumber1), ranges)))
+	fmt.Println(common.Sum(common.Map(common.Curry(checkRange, checkNumber2), ranges)))
 }
 
-func checkRange(p pair) int {
+func checkRange1(p pair) int {
 	var result int
 	for i := p[0]; i <= p[1]; i++ {
-		if checkNumber(i) {
+		if checkNumber1(i) {
 			result += i
 		}
 	}
@@ -26,7 +26,18 @@ func checkRange(p pair) int {
 	return result
 }
 
-func checkNumber(n int) bool {
+func checkRange(checkFunc func(int) bool, p pair) int {
+	var result int
+	for i := p[0]; i <= p[1]; i++ {
+		if checkFunc(i) {
+			result += i
+		}
+	}
+
+	return result
+}
+
+func checkNumber1(n int) bool {
 	num := strconv.Itoa(n)
 	if len(num)%2 > 0 {
 		return false
@@ -34,6 +45,20 @@ func checkNumber(n int) bool {
 
 	left, right := num[:len(num)/2], num[len(num)/2:]
 	return left == right
+}
+
+func checkNumber2(n int) bool {
+	num := strconv.Itoa(n)
+	for i := 1; i <= len(num)/2; i++ {
+		chunk := num[:i]
+		count := len(num) / i
+		repeated := strings.Repeat(chunk, count)
+		if repeated == num {
+			return true
+		}
+	}
+
+	return false
 }
 
 func getRanges() []pair {

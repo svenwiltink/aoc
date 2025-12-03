b756e4b4e52501be5228d9aea111f61641754396
Sven Wiltink <swiltink@transip.nl>
2025-12-03 10:20:07 +0100

    who doesn't love dot imports

--- 2025/03/go/main.go
+++ 2025/03/go/main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/svenwiltink/aoc/common"
+	. "github.com/svenwiltink/aoc/common"
 )
 
 func main() {
-	lines := common.GetLines()
-	numbers := common.Map(common.Curry(common.ExtractNumbersSep, ""), lines)
-	fmt.Println(common.Sum(common.Map(common.Curry(getDigit, 2), numbers)))
-	fmt.Println(common.Sum(common.Map(common.Curry(getDigit, 12), numbers)))
+	lines := GetLines()
+	numbers := Map(Curry(ExtractNumbersSep, ""), lines)
+	fmt.Println(Sum(Map(Curry(getDigit, 2), numbers)))
+	fmt.Println(Sum(Map(Curry(getDigit, 12), numbers)))
 }
 
 func getDigit(count int, numbers []int) int {
 	result := 0
 	for i := 1; i <= count; i++ {
-		index, digit := common.Max(numbers[:len(numbers)-(count-i)])
+		index, digit := Max(numbers[:len(numbers)-(count-i)])
 		numbers = numbers[index+1:]
 		result *= 10
 		result += digit

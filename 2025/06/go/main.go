334563e4211a619dbb9f96105edb2153636e908e
Sven Wiltink <swiltink@transip.nl>
2025-12-06 20:02:24 +0100

    p2

--- 2025/06/go/main.go
+++ 2025/06/go/main.go
@@ -8,6 +8,59 @@ import (
 )
 
 func main() {
+	part1()
+	part2()
+}
+
+func part2() {
+	lines := common.GetLines()
+	matrix := common.Map(func(k string) []rune {
+		return []rune(k)
+	}, lines)
+
+	matrix = common.Transpose(matrix)
+	start := true
+	operation := '0'
+	result := 0
+	row := 0
+	for _, l := range matrix {
+		// row is empty, add result
+		oString := strings.TrimSpace(string(l))
+		if len(oString) == 0 {
+			start = true
+			result += row
+			row = 0
+			continue
+		}
+
+		if start {
+			start = false
+			l, operation = l[:len(l)-1], l[len(l)-1]
+			var err error
+			row, err = strconv.Atoi(strings.TrimSpace(string(l)))
+			if err != nil {
+				panic(err)
+			}
+			continue
+		}
+
+		num := common.Must(strconv.Atoi, strings.TrimSpace(string(l)))
+
+		switch operation {
+		case '*':
+			row *= num
+		case '+':
+			row += num
+		}
+
+		start = false
+	}
+
+	result += row
+	fmt.Println(result)
+}
+
+func part1() {
 	lines := common.GetLines()
 	matrix := common.Map(strings.Fields, lines)
 	matrix = common.Transpose(matrix)
